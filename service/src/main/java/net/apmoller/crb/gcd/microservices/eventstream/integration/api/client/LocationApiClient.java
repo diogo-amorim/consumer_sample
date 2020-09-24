1f70d87f87d906ac0486958f9988f0965065a12f
Diogo Amorim <diogo.amorim@maersk.com>
2020-09-24 08:37:07 +0200

    clean commit

--- service/src/main/java/net/apmoller/crb/gcd/microservices/eventstream/integration/api/client/LocationApiClient.java
+++
@@ -1,8 +0,0 @@
-package net.apmoller.crb.gcd.microservices.eventstream.integration.api.client;
-
-import net.apmoller.crb.gcd.microservices.eventstream.integration.entity.Timezone;
-import reactor.core.publisher.Mono;
-
-public interface LocationApiClient {
-	public Mono<Timezone> getTimezone(String citycode);
-}
